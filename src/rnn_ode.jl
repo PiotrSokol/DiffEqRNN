b204a3b0b8dea2bca3dfd5b4b6eae4d7572281fe
Piotr <piotr.sokol@stonybrook.edu>
2020-09-29 20:16:53 -0400

    bit meandering
    Signed-off-by: Piotr <piotr.sokol@stonybrook.edu>

--- src/rnn_ode.jl
+++ src/rnn_ode.jl
@@ -1,4 +1,5 @@
 include("../layers.jl")
+import Interpolations: degree, NoInterp, itpflag, tcollect, Constant, Linear, Quadratic, Cubic
 import DiffEqFlux:NeuralDELayer, basic_tgrad
 
 struct RNNODE{M<:AbstractRNNDELayer,P,RE,T,A,K,S,I} <: NeuralDELayer
@@ -21,26 +22,19 @@ struct RNNODE{M<:AbstractRNNDELayer,P,RE,T,A,K,S,I} <: NeuralDELayer
             typeof(tspan),typeof(args),typeof(kwargs)}(
             model,p,re,tspan,args,kwargs)
     end
-    # function NeuralODE(model::FastChain,tspan,args...;p = initial_params(model),kwargs...)
-    #     re = nothing
-    #     new{typeof(model),typeof(p),typeof(re),
-    #         typeof(tspan),typeof(args),typeof(kwargs)}(
-    #         model,p,re,tspan,args,kwargs)
-    # end
 end
 
 function (n::NeuralODE)(x,p=n.p)
+    if ~isa(itp, Interpolations.AbstractInterpolation)
+    elseif isa(degree(filter(x -> ~isa(x, NoInterp), tcollect(itpflag, itp))[1]), Constant)
+    elseif isa(degree(filter(x -> ~isa(x, NoInterp), tcollect(itpflag, itp))[1]), Linear)
+    elseif isa(degree(filter(x -> ~isa(x, NoInterp), tcollect(itpflag, itp))[1]), Union{Quadratic,Cubic})
+    end
+
     dudt_(u,p,t) = n.re(p)(u)
     ff = ODEFunction{false}(dudt_,tgrad=basic_tgrad)
     prob = ODEProblem{false}(ff,x,getfield(n,:tspan),p)
     sense = InterpolatingAdjoint(autojacvec=ZygoteVJP())
     solve(prob,n.args...;sense=sense,n.kwargs...)
 end
-
-# function (n::NeuralODE{M})(x,p=n.p) where {M<:FastChain}
-#     dudt_(u,p,t) = n.model(u,p)
-#     ff = ODEFunction{false}(dudt_,tgrad=basic_tgrad)
-#     prob = ODEProblem{false}(ff,x,n.tspan,p)
-#     sense = InterpolatingAdjoint(autojacvec=ZygoteVJP())
-#     solve(prob,n.args...;sensealg=sense,n.kwargs...)
 end
