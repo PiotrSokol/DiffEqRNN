03aa498c12587706645171dc75e04c5a50da427b
Piotr <piotr.sokol@stonybrook.edu>
2020-11-12 20:11:54 -0500

    another big push
    Signed-off-by: Piotr <piotr.sokol@stonybrook.edu>

--- src/debugging.jl
+++ src/debugging.jl
@@ -1,13 +1,11 @@
-using Plots
+using DiffEqFlux, OrdinaryDiffEq, Flux, Optim
 include("/Users/piotrsokol/Documents/RNNODE.jl/src/rnn_ode.jl")
+ipt = sqrt(1/2)randn(Float32,3,100)
+lpt = LinearInterpolationFixedGrid(ipt);
+cpt = ConstantInterpolationFixedGrid(ipt);
+spt = CubicSplineFixedGrid(ipt);
 ∂nn = ∂GRUCell(1,2)
-node = RNNODE(∂nn, [0.f0,100.f0])
-u₀ = node.u₀
-sol = node(rand(eltype(u₀), 2,1));
-
-ipt = randn(2,100)
-Lipt = LinearInterpolationFixedGrid(ipt);
-Constipt = ConstantInterpolationFixedGrid(ipt);
-Cubipt = CubicSplineFixedGrid(ipt);
-
-node(Lipt)
+tspan = [0.f0,100.f0]
+tsteps = collect(tspan[1] : tspan[2])
+node = RNNODE(∂nn, [0.f0,100.f0], saveat=tsteps);
+sol = node(node.u₀);
